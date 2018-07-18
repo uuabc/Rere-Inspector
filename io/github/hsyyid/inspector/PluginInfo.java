d3e249c57979869cbe7cf90bfb8d24ce20947bf8
uuabc <32217735+uuabc@users.noreply.github.com>
2018-07-18 10:27:12 +0800

    Create PluginInfo.java

---
+++ io/github/hsyyid/inspector/PluginInfo.java
@@ -0,0 +1,10 @@
+package io.github.hsyyid.inspector;
+
+public abstract class PluginInfo
+{
+    public static final String ID = "inspector";
+    public static final String NAME = "Inspector";
+    public static final String VERSION = "0.7-rere";
+    public static final String DESCRIPTION = "This plugin enables servers to monitor griefing and provides rollback.";
+    public static final String WARNING = " if any problem occured post issues here: http://www.mcbbs.net/forum.php?mod=redirect&goto=findpost&ptid=660997&pid=12750382";
+}
