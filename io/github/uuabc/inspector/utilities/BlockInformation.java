1925802cde8c8d30f3451042d58cf1c85768be20
uuabc <32217735+uuabc@users.noreply.github.com>
2018-07-30 17:34:51 +0800

    Update BlockInformation.java

--- io/github/uuabc/inspector/utilities/BlockInformation.java
+++ io/github/uuabc/inspector/utilities/BlockInformation.java
@@ -3,24 +3,14 @@ package io.github.uuabc.inspector.utilities;
 import org.spongepowered.api.world.*;
 import org.spongepowered.api.block.*;
 
-import java.sql.Timestamp;
-
 public class BlockInformation
 {
     private Location<World> location;
     private BlockState oldBlockSnapshot;
-    private BlockState newBlockSnapshot;
-    private Timestamp timeEdited;
-    private String playerUUID;
-    private String playerName;
     
-    public BlockInformation(final Location<World> location, final BlockState oldBlockSnapshot, final BlockState newBlockSnapshot, final Timestamp timeEdited, final String playerUniqueId, final String playerName) {
+    public BlockInformation(final Location<World> location, final BlockState oldBlockSnapshot) {
         this.location = location;
         this.oldBlockSnapshot = oldBlockSnapshot;
-        this.newBlockSnapshot = newBlockSnapshot;
-        this.timeEdited = timeEdited;
-        this.playerUUID = playerUniqueId;
-        this.playerName = playerName;
     }
     
     public Location<World> getLocation() {
@@ -29,21 +19,5 @@ public class BlockInformation
     
     public BlockState getOldBlockSnapshot() {
         return this.oldBlockSnapshot;
-    }
-    
-    public BlockState getNewBlockSnapshot() {
-        return this.newBlockSnapshot;
-    }
-    
-    public Timestamp getTimeEdited() {
-        return this.timeEdited;
-    }
-    
-    public String getPlayerUUID() {
-        return this.playerUUID;
-    }
-    
-    public String getPlayerName() {
-        return this.playerName;
     }
 }
